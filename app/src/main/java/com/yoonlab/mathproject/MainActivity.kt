7ada15e8f40fadfc10a91b77a47d0fb4709949a0
KimWash <ckm0728wash@gmail.com>
2020-03-02 14:04:05 +0900

    첫실행 감지 및 IntroduceActivity로 이동

--- app/src/main/java/com/yoonlab/mathproject/MainActivity.kt
+++ app/src/main/java/com/yoonlab/mathproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.yoonlab.mathproject
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,9 +9,21 @@ import com.yoonlab.mathproject.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
+    fun checkFirstRun(){
+        var prefs:SharedPreferences = getSharedPreferences("Pref", MODE_PRIVATE)
+        var isFirstRun = prefs.getBoolean("isFirstRun", true)
+        if(isFirstRun){
+            val intIntent = Intent(this@MainActivity, IntroduceActivity::class.java)
+            startActivity(intIntent)
+            prefs.edit().putBoolean("isFirstRun", false).apply()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        checkFirstRun()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         var loginButton = findViewById<Button>(R.id.loginButton) // 버튼 선언
         loginButton.setOnClickListener { View -> val loginIntent = Intent(this@MainActivity, LoginActivity::class.java); startActivity(loginIntent) } //버튼클릭 리스너 선언
     }
