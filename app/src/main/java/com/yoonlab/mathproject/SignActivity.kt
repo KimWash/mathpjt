e0a3344e444507ea5b109e097411c3005915c707
Dainel03 <35254304+Dainel03@users.noreply.github.com>
2020-03-03 19:25:58 +0900

    patch
    patch

--- app/src/main/java/com/yoonlab/mathproject/SignActivity.kt
+++ app/src/main/java/com/yoonlab/mathproject/SignActivity.kt
@@ -6,12 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.yoonlab.mathproject.SignActivity
+import com.yoonlab.mathproject.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_sign.*
 
 class SignActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
+        var register = findViewById<Button>(R.id.resister)
+        var signin = findViewById<Button>(R.id.signbutton)
+        val registerpage = Intent(this@SignActivity, JoinActivity::class.java)
+        register.setOnClickListener{View -> startActivity(registerpage)}
+        val signpage = Intent(this@SignActivity, LoginActivity::class.java)
+        signin.setOnClickListener{View -> startActivity(signpage)}
+
 
 
 
