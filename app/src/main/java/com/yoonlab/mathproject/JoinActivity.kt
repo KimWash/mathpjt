b465cba5563382e15906323dd9e7868a80813a3d
KimWash <ckm0728wash@gmail.com>
2020-03-03 23:31:52 +0900

    JoinActivity 수정

--- app/src/main/java/com/yoonlab/mathproject/JoinActivity.kt
+++ app/src/main/java/com/yoonlab/mathproject/JoinActivity.kt
@@ -1,4 +1,18 @@
 package com.yoonlab.mathproject
 
-class JoinActivity {
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.yoonlab.mathproject.SignActivity
+import com.yoonlab.mathproject.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_sign.*
+
+class JoinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
 }
