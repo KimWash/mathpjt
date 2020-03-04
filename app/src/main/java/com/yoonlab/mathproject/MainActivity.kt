7d5868fd90c28b3af6133957249a275cfdb72449
Dainel03 <35254304+Dainel03@users.noreply.github.com>
2020-03-04 15:39:24 +0900

    I'm stressed

--- app/src/main/java/com/yoonlab/mathproject/MainActivity.kt
+++ app/src/main/java/com/yoonlab/mathproject/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.yoonlab.mathproject
 
+import android.content.Context
+import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.yoonlab.mathproject.ui.login.LoginActivity
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,7 +43,29 @@ class MainActivity : AppCompatActivity() {
         checkFirstRun()
         nightMode()
         setContentView(R.layout.activity_main)
+    }
 
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+        settingbutton.setOnClickListener{
+            showingSettingPop()
+        }
+
+    }
+    private fun showingSettingPop(){
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.activity_setpop,null)
+        val textView: TextView = view.findViewById(R.id.settingmenu)
+        textView.text = "Test중입니다"
+
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("Setting")
+            .setPositiveButton("저장"){dialog, which ->
+                Toast.makeText(applicationContext, "Save", Toast.LENGTH_SHORT).show()
+            }
+        alertDialog.setView(view)
+        alertDialog.show()
 
     }
 
