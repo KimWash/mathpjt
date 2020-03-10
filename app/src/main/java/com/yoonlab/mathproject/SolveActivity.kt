656c1f4b26941d83412fc0c68d254fdf3abebcf5
Dainel03 <35254304+Dainel03@users.noreply.github.com>
2020-03-11 00:49:36 +0900

    delete module

--- app/src/main/java/com/yoonlab/mathproject/SolveActivity.kt
+++ app/src/main/java/com/yoonlab/mathproject/SolveActivity.kt
@@ -23,7 +23,6 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
-import java.io.IOException
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.reward.RewardedVideoAd
 
