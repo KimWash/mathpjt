56849d262dfb2329873a9f32a5939ca922559f9c
Dainel03 <35254304+Dainel03@users.noreply.github.com>
2020-03-12 19:01:08 +0900

    오류 처치

--- app/src/main/java/com/yoonlab/mathproject/StoreActivity.kt
+++ app/src/main/java/com/yoonlab/mathproject/StoreActivity.kt
@@ -1,10 +1,12 @@
 package com.yoonlab.mathproject
 
 import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.app.Activity
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_store.*
 
 
@@ -13,10 +15,8 @@ class StoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_store)
-        var homeButton = findViewById<Button>(R.id.home)
-        val intIntent = Intent(this@StoreActivity, MainActivity::class.java)
-        homeButton.setOnClickListener{View -> startActivity(intIntent)}
+        val homepage = Intent(this@StoreActivity, MainActivity::class.java)
+        home.setOnClickListener{View -> startActivity(homepage)}
     }
-
 
 }
